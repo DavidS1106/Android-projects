5919c4f9efd8fa39e8f9a26e43014ffb602b103e
Sabo David <58429655+DavidS1106@users.noreply.github.com>
2021-05-14 15:16:41 +0200

    Update MainActivity.java
    .

--- MVP/java/be/ipl/androidversions/MainActivity.java
+++ MVP/java/be/ipl/androidversions/MainActivity.java
@@ -32,19 +32,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+       
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -59,27 +56,7 @@ public class MainActivity extends AppCompatActivity {
         private final MainActivity mParentActivity;
         private final List<AndroidVersion> mValues;
 
-    /*    private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AndroidVersion item = (AndroidVersion) view.getTag();
-                if (mTwoPane) {
-                    Bundle arguments = new Bundle();
-                    arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.id);
-                    ItemDetailFragment fragment = new ItemDetailFragment();
-                    fragment.setArguments(arguments);
-                    mParentActivity.getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.item_detail_container, fragment)
-                            .commit();
-                } else {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, ItemDetailActivity.class);
-                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item.id);
-
-                    context.startActivity(intent);
-                }
-            }
-        };*/
+    
 
         SimpleItemRecyclerViewAdapter(MainActivity parent,
                                       List<AndroidVersion> items) {
@@ -100,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
             holder.tvVersionNum.setText(mValues.get(position).getVersion());
             holder.ivVersionImage.setImageResource(mValues.get(position).getImageId());
             holder.itemView.setTag(mValues.get(position));
-           // holder.itemView.setOnClickListener(mOnClickListener);
+           
         }
 
         @Override
